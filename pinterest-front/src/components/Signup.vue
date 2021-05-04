827f92527504eb649f8ccd8c2d789e992da2062c
sm tamizhazhagan <tamilcrea@sms-MacBook-Air.local>
2021-05-04 20:03:44 +0530

    fixed signup

--- pinterest-front/src/components/Signup.vue
+++ pinterest-front/src/components/Signup.vue
@@ -2,13 +2,13 @@
 
  <div class="register-form">
      <h1> Welcome to  the pinterest clone </h1>
-     <form  class="box" v-on:click="formSubmit" id="signup">
+     <form  class="box"  id="signup">
          <div class="columns">
              <div class="column m-md-6">
         <div class="field">  
          <div class="control">
              <label class="label"> Firstname </label>
-             <input class="input"  v-model="user.first_name" type="text"  placeholder="enter the firstname" >
+             <input class="input"  v-model="first_name" type="text"  placeholder="enter the firstname" >
          </div>
         </div>
          </div>
@@ -16,7 +16,7 @@
        <div class="field">
          <div class="control">
              <label class="label"> lastname </label>
-             <input  class="input" v-model="user.last_name" type="text" placeholder="enter the lastname" >
+             <input  class="input" v-model="last_name" type="text" placeholder="enter the lastname" >
          </div>
         </div>
         </div>
@@ -26,7 +26,7 @@
          <div class="field">
             <div class="control">
              <label class="label"> Phone number </label>
-             <input  class="input" v-model="user.phone" type="tel"  placeholder="9123456789" >
+             <input  class="input" v-model="phone" type="tel"  placeholder="9123456789" >
            </div>
          </div>
          </div>
@@ -34,19 +34,19 @@
          <div class="field">
          <div class="control">
              <label class="label"> Email </label>
-             <input class="input" v-model="user.email" type="email"  placeholder="abcd@gmail.com" >
+             <input class="input" v-model="email" type="email"  placeholder="abcd@gmail.com" >
           </div>
          </div>
          </div>
          </div>
           <label class="label"> Date of birth </label>
-         <input  v-model="user.dob" class="input" type="date">
+         <input  v-model="dob" class="input" type="date">
          <div class="columns">
             <div class="column m-md-5">
          <div class="field">
             <div class="control">
              <label class="label"> Password </label>
-             <input class="input" v-model="user.password" type="password" >
+             <input class="input" v-model="password" type="password" >
          </div>
         </div>
             </div>
@@ -54,13 +54,14 @@
             <div class="field">
           <div class="control">
              <label class="label"> Confirm password </label>
-             <input class="input" v-model="user.confirm_password" type="password" >
+             <input class="input" v-model="confirm_password" type="password" >
          </div>
       </div>
          </div>
          </div>
-         <button type="button" v-on:click="setAccount"   class="button is-info"> Create account </button>
-         <flash-message class="myCustomClass"></flash-message>
+
+       
+         <button type="button"  v-on:click="formSubmit" class="button is-info"> Create account </button>
      </form>
      
   </div>
@@ -69,18 +70,11 @@
 
 <script>
  import axios from 'axios'
-import Vue from 'vue';
-import FlashMessage from '@smartweb/vue-flash-message';
-Vue.use(FlashMessage);
-
-//    Vue.component('multiselect', Multiselect)
 
 export default {
-
     name: 'Signup',
     data() {
         return {
-            user: {
                first_name: '',
                last_name: '',
                email: '',
@@ -88,47 +82,35 @@ export default {
                password: '',
               confirm_password: '',
               dob: '',
-              article_preferences: [],
-            },    
-        }
+            }    
         
     },
 
-    updated(){
-           
-           this.setAccount();
-    },
     mounted() {
         this.formSubmit();
     },
 
-        methods : {
-          formSubmit() {
-              axios.post('/users', this.user)
-            .then(res => (this.user = res.data))
-            .catch((error) => {
-                console.log(error)
-                })
-                if(status) {
-                        this.flashMessage.success({
-                            title: 'Don\'t Warry',
-                            message: 'Be Happy!',
-                            time: 1000,
-                            flashMessageStyle: {
-                                backgroundColor: 'linear-gradient(#e66465, #9198e5)'
-                            }
-                        });
-                    }
-            },
+   
 
-            setAccount() {
-                 if(this.user.length==1)
-                { 
-                    this.$router.push('/article')
-                     
-                }
-            }
-        },  
+        methods : {
+
+           async formSubmit() {
+                           await axios.post('/users',{
+
+            first_name: this.first_name,
+            last_name: this.last_name,
+            email: this.email,
+            phone: this.phone,
+            password: this.password,
+            confirm_password: this.confirm_password,
+            dob: this.dob });
+
+             this.$router.push('/login');
+            
+          
+          },
+        }
+          
 }
 </script>
 
