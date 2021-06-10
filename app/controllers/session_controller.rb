2362e5a6f3b98f083a0de1c0b8609046304c9740
tamilz <tamilcrea@gmail.com>
2021-06-10 10:50:42 +0530

    Update session_controller.rb

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,6 +2,8 @@ class SessionController < ApplicationController
   
   def create
     user = User.where(email: params[:email], password: params[:password])
+    
+    session[:user_id] = user.id
     if user.present?
       render json: user
     else
